9278861ab7ae5434e5004792e00013667e5f655c
Jepe <heruiyang@outlook.com>
2019-11-22 17:31:00 +0800

    15-19

---
+++ problem/letterCombinations.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        this->map = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+        if (digits.length() == 0)
+            return {};
+        this->mLevel = digits.length();
+        this->digits = digits;
+        dfs("", 0);
+        return this->result;
+    }
+
+private:
+    void dfs(string res, int l)
+    {
+        if (l == this->mLevel)
+        {
+            result.push_back(res);
+        }
+        char n = this->digits[l];
+        string s = map[n];
+        for (auto &&i : s)
+        {
+            dfs(res + i, l + 1);
+        }
+    }
+    string digits;
+    int mLevel;
+    vector<string> result;
+    unordered_map<char, string> map;
+};
+
+int main()
+{
+    Solution solution;
+    string s = "23";
+    auto result = solution.letterCombinations(s);
+    for (auto &&i : result)
+    {
+        cout << i << endl;
+    }
+}
---
+++ problem/threeSumClosest.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        if (nums.size() < 3)
+            return 0;
+        int result = 0, g = INT_MAX;
+        int length = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < length; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int l = i + 1;
+            int r = length - 1;
+            while (l < r)
+            {
+                int tmp = nums[i] + nums[l] + nums[r];
+                if (abs(tmp - target) < g)
+                {
+                    g = abs(tmp - target);
+                    result = tmp;
+                }
+                if (tmp > target)
+                {
+                    r--;
+                }
+                else if (tmp < target)
+                {
+                    l++;
+                }
+                else if (tmp == target)
+                {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<int> nums({1,1,1});
+    int t = 3;
+    auto result = solution.threeSumClosest(nums, t);
+    cout << result;
+}
